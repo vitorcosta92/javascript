4e6211f996e590cb37ed5bee41a3ebe525af9b39
Vitor Costa <vcostapaula@gmail.com>
2026-07-13 21:22:31 -0300

    Exercício Tabuada

--- exercícios/ex019/ex019/script.js
+++ exercícios/ex019/ex019/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function gerar() {
+    let numero = document.getElementById('txtnum')
+    let tabuada = document.getElementById('tabuada')
+
+    if 
+    (numero.value.length == 0) {
+        window.alert('[ERRO] Digite um número, meu parça!')
+    } else {
+        let n = Number(numero.value)
+        tabuada.innerHTML = ''
+
+        for (let c = 1; c <= 10; c += 1) {
+            tabuada.innerHTML += `<option>${n} x ${c} = ${n*c}</option>`
+        }
+    } 
+}
+
+
